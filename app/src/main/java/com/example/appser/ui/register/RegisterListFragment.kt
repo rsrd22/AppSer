87d91bcf967333abf4384edc47fa6cddaf43b84d
rramosd <rramosd@unimagdalena.edu.co>
2022-04-25 16:21:56 -0500

    Layout Home and Register

---
+++ app/src/main/java/com/example/appser/ui/register/RegisterListFragment.kt
@@ -0,0 +1,15 @@
+package com.example.appser.ui.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.appser.R
+
+class RegisterListFragment : Fragment(R.layout.fragment_register_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}

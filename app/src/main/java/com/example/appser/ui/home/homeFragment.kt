9a5108ec1f6bc0e78c36b1bec51fd2639532ff2d
Richard Ramos Duran <rsrd22@gmail.com>
2022-04-26 13:06:06 -0500

    Conexion Pantalla Registro

--- app/src/main/java/com/example/appser/ui/home/homeFragment.kt
+++ app/src/main/java/com/example/appser/ui/home/homeFragment.kt
@@ -5,12 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.appser.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class homeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val btnLogin = view.findViewById<Button>(R.id.btn_Login)
+        val btnRegistrarse = view.findViewById<TextView>(R.id.txt_Registrarse)
+        val btnLista = view.findViewById<TextView>(R.id.txt_Lista)
 
+        btnLogin.setOnClickListener{
+            val txtUsuario = view.findViewById<EditText>(R.id.txt_usuario)
+            Toast.makeText(requireContext(), "Usuario: ${txtUsuario.text}", Toast.LENGTH_LONG).show()
+        }
+        btnRegistrarse.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_registerFragment2)
+        }
+        btnLista.setOnClickListener{
+            findNavController().navigate(R.id.action_homeFragment_to_registerListFragment2)
+        }
     }
 }

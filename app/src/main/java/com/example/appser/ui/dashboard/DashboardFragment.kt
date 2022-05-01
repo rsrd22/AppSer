b31e8ce8958296fa239bb23ee829797bc219b8b5
kennys <kgdc13@gmail.com>
2022-05-01 01:15:34 -0500

    Vista dashboard layout

---
+++ app/src/main/java/com/example/appser/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,16 @@
+package com.example.appser.ui.dashboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.appser.R
+
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}

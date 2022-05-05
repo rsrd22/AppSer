e8dd085358d3281d4560a11e9c0df4f309570da5
rramosd <rramosd@unimagdalena.edu.co>
2022-05-05 09:43:08 -0500

    View Model Cuestionario Preguntas

--- app/src/main/java/com/example/appser/presentation/CuestionarioPreguntasViewModel.kt
+++ app/src/main/java/com/example/appser/presentation/CuestionarioPreguntasViewModel.kt
@@ -2,10 +2,41 @@ package com.example.appser.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.example.appser.core.Resource
+import com.example.appser.data.model.CuestionarioPreguntasEntity
 import com.example.appser.repository.CuestionarioPreguntasRepository
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
 
 class CuestionarioPreguntasViewModel(private val repo: CuestionarioPreguntasRepository): ViewModel() {
 
+    fun fetchAllCuestionarioPreguntas() = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try{
+            emit(Resource.Success(repo.getAllCuestionarioPreguntas()))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+
+    fun fetchCuestionarioPreguntasbyId(id: Long) = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try{
+            emit(Resource.Success(repo.getCuestionarioPreguntabyId(id)))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+
+    fun fetchSaveCuestionarioPregunta(cuestionariopregunta: CuestionarioPreguntasEntity) = liveData(Dispatchers.IO) {
+        emit(Resource.Loading())
+        try{
+            emit(Resource.Success(repo.saveCuestionarioPregunta(cuestionariopregunta)))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
 
 
 }

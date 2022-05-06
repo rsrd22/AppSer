8391b102eec100186988851c337125506e00f6cd
kennys <kgdc13@gmail.com>
2022-05-06 07:35:50 -0500

    Intento de DrawerNav

--- app/src/main/java/com/example/appser/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/appser/ui/dashboard/DashboardFragment.kt
@@ -1,27 +1,42 @@
 package com.example.appser.ui.dashboard
 
+import android.app.ActionBar
 import android.content.Context
 import android.content.Intent
+import android.icu.lang.UCharacter
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.content.res.ColorStateListInflaterCompat.inflate
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 //import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import com.example.appser.R
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.activity_menu_test.*
+import kotlinx.android.synthetic.main.app_bar_menu_test.*
 
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
         setHasOptionsMenu(true)
         super.onViewCreated(view, savedInstanceState)
  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+       // var drawerLayout : DrawerLayout77777777777777777777777777777777777777-swt5r_open)
+        //drawer_layout.addDrawerListener(toggle)
+        //toggle.syncState()
+
+
     }
 
 

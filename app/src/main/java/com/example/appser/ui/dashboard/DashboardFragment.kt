fd7241a9368011ec1911e1251c24642382a53c65
Richard Ramos Duran <rsrd22@gmail.com>
2022-06-03 02:05:56 -0500

    lo que tu hiciste mas solución del menú duplicado

--- app/src/main/java/com/example/appser/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/appser/ui/dashboard/DashboardFragment.kt
@@ -3,6 +3,7 @@ package com.example.appser.ui.dashboard
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -16,6 +17,7 @@ import com.example.appser.data.local.AppDatabase
 import com.example.appser.data.model.EmocionesList
 import com.example.appser.data.model.relations.CategoriasWithPreguntas
 import com.example.appser.data.model.relations.PersonaAndUsuario
+import com.example.appser.data.preference.SerApplication.Companion.prefs
 import com.example.appser.data.resource.CategoriasDataSource
 import com.example.appser.data.resource.CicloVitalDataResource
 import com.example.appser.data.resource.EmocionesDataSource
@@ -24,6 +26,8 @@ import com.example.appser.presentation.*
 import com.example.appser.repository.CategoriasRepositoryImpl
 import com.example.appser.repository.CicloVitalRepositoryImpl
 import com.example.appser.repository.EmocionesRepositoryImpl
+import kotlinx.android.synthetic.main.activity_menu_test.*
+import kotlinx.android.synthetic.main.content_menu_test.*
 import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 
@@ -40,6 +44,11 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
         super.onCreate(savedInstanceState)
         appDatabase = AppDatabase.getDatabase(requireContext())
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadData()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -62,6 +71,10 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
         getAllCategorias()
         getAllEmociones()
+
+    }
+
+    private fun loadData(){
 
     }
 
@@ -177,34 +190,14 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
         findNavController().navigate(R.id.action_dashboardFragment_to_questionsFragment)
     }
 
-
-
-
     override fun setHasOptionsMenu(hasMenu: Boolean) {
         super.setHasOptionsMenu(hasMenu)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+    /*override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.main_menu, menu)
         super.onCreateOptionsMenu(menu, inflater)
 
-    }
+    }*/
 
-//override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    //  val id: Int = item.getItemId()
-    //if (id == R.id.identify_emotion) {
-
-    //}
-    //  return true
-    // lo ideal aquí sería hacer un intent para abrir una nueva clase como lo siguiente
-    //Log.i("ActionBar", "Settings!")
-    //val about = Intent(
-    //  ApplicationProvider.getApplicationContext<Context>(),
-    //About::class.java
-    //)
-    //startActivity(about)
-    // return true
-    //}
-    //return super.onOptionsItemSelected(item)
-//}
 }
